ecbfeea718a921a8ffbba9f12b1311ea0f53fc73
ilyabeliy <ya.ila.white@gmail.com>
2023-12-16 23:18:36 +0100

    upload files to gcp

--- main.py
+++ main.py
@@ -23,11 +23,16 @@ class ApiControl:
         :return: response from api
         """
 
-        request = requests.get(url=self.url + '/installs',
-                               headers=self.key,
-                               params={'date': date})
-        result = request.json()
-        return result
+        try:
+            request = requests.get(url=self.url + '/installs',
+                                   headers=self.key,
+                                   params={'date': date})
+            result = request.json()
+            return result
+
+        except Exception as ex:
+            logging.error("Something went wrong in 'get_installs'")
+            print(ex)
 
     def get_costs(self, date: str, dimension=None) -> requests.Response:
         """
@@ -43,17 +48,29 @@ class ApiControl:
         if dimension:
             params['dimensions'] = dimension
 
-        request = requests.get(url=self.url + '/costs',
-                               headers=self.key,
-                               params=params)
-        return request
+        logging.info("Costs request getting")
+
+        try:
+            request = requests.get(url=self.url + '/costs',
+                                   headers=self.key,
+                                   params=params)
+            return request
+
+        except Exception as ex:
+            logging.error("Something went wrong in 'ApiControl.get_costs'")
+            print(ex)
 
     def get_orders(self, date: str) -> requests.Response:
+        logging.info("Orders response getting")
+        try:
+            request = requests.get(url=self.url + '/orders',
+                                   headers=self.key,
+                                   params={'date': date})
+            return request
 
-        request = requests.get(url=self.url + '/orders',
-                               headers=self.key,
-                               params={'date': date})
-        return request
+        except Exception as ex:
+            logging.error('Something went wrong in "ApiControl.get_orders"')
+            print(ex)
 
     def get_events_next_page(self, date: str, next_page=None):
         params = {'date': date}
@@ -70,53 +87,44 @@ class ApiControl:
 class GCP:
     def __init__(self, url, key):
         self.api_cursor = ApiControl(url, key)
-        self.project_name = 'hly-gnss'
-        self.tables_names = ['CPI', 'Revenue', 'ROAS']
-        self.bucket_name = 'hly-gnss-bckt'
-        self.date = ((datetime.datetime.now() - datetime.timedelta(days=1))
+        self.FILE = 'DataMart_data/DataMart.csv'
+        self.PROJECT_NAME = 'hly-gnss'
+        self.TABLE_NAMES = ['cpi', 'revenue', 'roas']
+        self.BUCKET_NAMES = 'hly-gnss-bckt'
+        self.DATE = ((datetime.datetime.now() - datetime.timedelta(days=1))
                      .strftime('%Y-%m-%d'))
 
-    def installs_prepare(self):
-        # TODO change date
-        # TODO change csv return
+        self.data_frame_for_update = self.setup_existed_data()
 
-        installs_count = self.api_cursor.get_installs(self.date)['count']
-        # installs_records = self.api_cursor.get_installs(self.date)['records']
-        # normalized_installs = installs_records.replace("'", '"')
-        # normalized_installs = json.loads(normalized_installs)
-        # df = pd.DataFrame.from_dict(normalized_installs)
-        #
-        # return df
+        self.installs = self.api_cursor.get_installs(self.DATE)['count']
+
+    def setup_existed_data(self) -> pd.DataFrame:
+        client = storage.Client()
+        bucket = client.get_bucket(self.BUCKET_NAMES)
+        blob = bucket.blob(self.FILE)
+
+        if blob.exists():
+            file_content = blob.download_as_string()
+            data = io.StringIO(file_content.decode('utf-8'))
+            df = pd.read_csv(data)
+            return df
+        else:
+            return pd.DataFrame()
 
     def orders_prepare(self):
-        # TODO change date
-        # TODO change csv return
 
         parquet_table = pq.read_table(
-            io.BytesIO(controller.get_orders(self.date).content)
+            io.BytesIO(self.api_cursor.get_orders(self.DATE).content)
         )
         df = parquet_table.to_pandas()
         return df
 
-    def costs_prepare(self):
-        # TODO add more parametrs
-        # TODO change csv return
-
-        costs = self.api_cursor.get_costs(self.date).content
-        costs = costs.decode('utf-8')
-        rows = costs.split('\n')
-        columns = rows[0].split('\t')
-
-        data = {}
-
-        for row in rows[1:]:
-            values = row.split('\t')
-            if len(values) == 2:
-                channel = values[0]
-                cost = float(values[1])
-                data[channel] = cost
-
-        df = pd.DataFrame.from_dict(data, orient='index', columns=[columns[1]])
+    def costs_prepare(self) -> float:
+        costs = self.api_cursor.get_costs(self.DATE).content
+        data = costs.decode('utf-8')
+        data = data.split('\n')
+        costs = float(data[1])
+        return costs
 
     def events_prepare(self):
         # TODO change logic
@@ -124,7 +132,7 @@ class GCP:
         in_progres = True
         page = 0
         errors = 10
-        output = controller.get_events_next_page(self.date)
+        output = self.api_cursor.get_events_next_page(self.DATE)
         next_page = output['next_page']
         output = output['data']
 
@@ -138,7 +146,7 @@ class GCP:
                 output = pd.DataFrame(output)
                 print(output)
                 logging.info("Trying to get response data")
-                output = controller.get_events_next_page(self.date,
+                output = self.api_cursor.get_events_next_page(self.DATE,
                                                          next_page)
 
                 logging.info("Trying to get new page's link")
@@ -161,13 +169,68 @@ class GCP:
 
                 in_progres = False
 
-    def cpi_create_data(self):
-        data = {'date': self.date, 'instals': self.instals, 'costs': self.costs,
-                'cpi': self.costs/self.instals}
+    def cpi_data(self) -> int:
+        costs = self.costs_prepare()
+        data = costs / self.installs
+        return data
+
+    def revenue_data(self) -> int:
+        ...
+
+    def roas_data(self) -> int:
+        ...
+
+    def new_data(self) -> None:
+        logging.info("Setup data for update")
+        try:
+            new_data = {'date': [self.DATE],
+                        'cpi': [self.cpi_data()],
+                        'revenue': [self.revenue_data()],
+                        'roas': [self.roas_data()]}
+
+            pd.concat([self.data_frame_for_update, new_data])
+        except Exception as ex:
+            logging.error("Something went wrong in 'GCP.new_data'")
+            print(ex)
+
+    def upload_changes(self) -> None:
+        self.new_data()
+
+        logging.info("Connecting to storage")
+
+        try:
+            client = storage.Client()
+            bucket = client.get_bucket(self.BUCKET_NAMES)
+            blob = bucket.blob(self.FILE)
+            df = self.data_frame_for_update.to_csv(index=False)
+            blob.upload_from_string(df, 'text/csv')
+        except Exception as ex:
+            logging.error("Something went wrong in 'GCP.upload_changes'")
+            print(ex)
 
 
 if __name__ == '__main__':
     controller = ApiControl(URL, KEY)
-    print(controller.get_installs("2020-12-10"))
+    # data = {'date': [1], 'instals': [2], 'costs': [3], 'cpi': [4]}
+    # df = pd.DataFrame(data)
+    # print(df.to_csv())
+    data = {'date': ['2023-12-16', '2023-12-17'],
+            'cpi': [2222, 4434],
+            'revenue': [3333, 23434.333],
+            'roas': [234324, 345345]}
+
+    # Создание датафрейма
+    # df = pd.DataFrame(data)
+    # df = pd.DataFrame()
+    # print(df)
+    # new_data = {'date': ['2023-12-18'],
+    #         'cpi': [1],
+    #         'revenue': [1],
+    #         'roas': [1]}
+    # new_data = pd.DataFrame(new_data)
+    # print(pd.concat([df, new_data]))
+    #
+
+
 
 
